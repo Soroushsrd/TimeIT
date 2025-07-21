f162ce54aa7a605048d7745f8246ac7edaf582a6
Soroushsrd <Sardashtisoroosh@gmail.com>
2025-07-22 03:21:12 +0330

    Add(filewatcher): basic watcher for directory file modifications

---
+++ src/file_watcher.rs
@@ -0,0 +1,196 @@
+use std::{
+    collections::{HashMap, HashSet},
+    path::{Path, PathBuf},
+    time::{Duration, Instant},
+};
+
+use notify::Event;
+
+pub struct FileWatcher {
+    //TODO: use atomics? with rwlock?
+    last_events: HashMap<PathBuf, Instant>,
+    debouncing_duration: Duration,
+    source_extensions: HashSet<&'static str>,
+    ignore_patterns: Vec<&'static str>,
+}
+
+impl FileWatcher {
+    pub fn new() -> Self {
+        let source_extensions = HashSet::from([
+            ".rs", ".py", ".js", ".jsx", ".ts", ".tsx", ".go", ".cpp", ".h", ".c", ".lua",
+        ]);
+
+        let ignore_patterns = vec![
+            // Build artifacts
+            "/target/",
+            "/build/",
+            "/dist/",
+            "/out/",
+            "/.next/",
+            // Dependencies
+            "/node_modules/",
+            "/vendor/",
+            "/.venv/",
+            "/venv/",
+            "/env/",
+            // Version control
+            "/.git/",
+            "/.svn/",
+            "/.hg/",
+            // IDE/Editor files
+            "/.vscode/",
+            "/.idea/",
+            "/.vs/",
+            // Temporary files
+            "/tmp/",
+            "/temp/",
+            "/.tmp/",
+            // Cache directories
+            "/.cache/",
+            "/cache/",
+            "/__pycache__/",
+            "/.pytest_cache/",
+            // Log files
+            "/logs/",
+            "/.log/",
+        ];
+
+        Self {
+            last_events: HashMap::new(),
+            debouncing_duration: Duration::from_millis(100),
+            source_extensions,
+            ignore_patterns,
+        }
+    }
+
+    /// checks to see if the event received is actually a modification
+    /// type of event. if yes, then returns the path of the modified file
+    pub fn process_event(&mut self, event: &Event) -> Option<PathBuf> {
+        match event.kind {
+            notify::EventKind::Modify(notify::event::ModifyKind::Data(
+                notify::event::DataChange::Any,
+            )) => {
+                // will check for keyboard movement later
+            }
+            _ => return None,
+        }
+
+        for path in &event.paths {
+            if self.should_ignore(path) {
+                continue;
+            }
+
+            if self.should_debounce(path) {
+                continue;
+            }
+
+            return Some(path.clone());
+        }
+        None
+    }
+
+    /// checks and sees if the diff in time between the new event
+    /// and last event is less than debouncing duration.
+    /// also saves the new event instant if time diff is
+    /// more than debouncing duration
+    fn should_debounce(&mut self, path: &Path) -> bool {
+        let now = Instant::now();
+        let path = path.to_path_buf();
+        if let Some(last_event) = self.last_events.get(&path) {
+            if now.duration_since(*last_event) < self.debouncing_duration {
+                return true;
+            }
+        }
+        self.last_events.insert(path, now);
+        false
+    }
+
+    // checks and sees if the path contains modifications
+    // in files that was mentioned in ignored paths.
+    // this should later be used to check .ignore files(?)
+    fn should_ignore(&self, path: &Path) -> bool {
+        let path_str = path.to_string_lossy();
+        for pattern in &self.ignore_patterns {
+            if path_str.contains(pattern) {
+                return true;
+            }
+        }
+
+        // checking file extensions
+        if let Some(extension) = path.extension() {
+            if let Some(ext_str) = extension.to_str() {
+                let ext_with_dot = format!(".{}", ext_str);
+                if self.source_extensions.contains(ext_with_dot.as_str()) {
+                    return false;
+                }
+            }
+        }
+        true
+    }
+}
+
+// TODO: use an enum
+pub fn detect_language(path: &Path) -> Option<&'static str> {
+    if let Some(extension) = path.extension() {
+        if let Some(ext_str) = extension.to_str() {
+            match ext_str {
+                "rs" => Some("Rust"),
+                "py" => Some("Python"),
+                "js" => Some("JavaScript"),
+                "ts" => Some("TypeScript"),
+                "jsx" => Some("JavaScript React"),
+                "tsx" => Some("TypeScript React"),
+                "go" => Some("Go"),
+                "cpp" | "cc" | "cxx" => Some("C++"),
+                "c" => Some("C"),
+                "h" | "hpp" => Some("C/C++ Header"),
+                "java" => Some("Java"),
+                "kt" => Some("Kotlin"),
+                "php" => Some("PHP"),
+                "rb" => Some("Ruby"),
+                "swift" => Some("Swift"),
+                "scala" => Some("Scala"),
+                "clj" => Some("Clojure"),
+                "hs" => Some("Haskell"),
+                "elm" => Some("Elm"),
+                "dart" => Some("Dart"),
+                "lua" => Some("Lua"),
+                "vim" => Some("Vim Script"),
+                "sh" | "bash" => Some("Bash"),
+                "zsh" => Some("Zsh"),
+                "fish" => Some("Fish"),
+                "ps1" => Some("PowerShell"),
+                "sql" => Some("SQL"),
+                "html" => Some("HTML"),
+                "css" => Some("CSS"),
+                "scss" => Some("SCSS"),
+                "sass" => Some("Sass"),
+                "less" => Some("Less"),
+                "md" => Some("Markdown"),
+                "yml" | "yaml" => Some("YAML"),
+                "toml" => Some("TOML"),
+                "json" => Some("JSON"),
+                "xml" => Some("XML"),
+                _ => Some("Unknown"),
+            }
+        } else {
+            None
+        }
+    } else {
+        // handling files without extensions
+        if let Some(filename) = path.file_name() {
+            if let Some(name_str) = filename.to_str() {
+                match name_str {
+                    "Dockerfile" => Some("Docker"),
+                    "Makefile" => Some("Makefile"),
+                    "CMakeLists.txt" => Some("CMake"),
+                    _ => Some("Text"),
+                }
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,32 @@
+mod file_watcher;
+use file_watcher::{FileWatcher, detect_language};
+use notify::{Event, RecursiveMode, Result, Watcher};
+use std::path::Path;
+use std::sync::mpsc;
+
+fn main() -> Result<()> {
+    println!("Starting filtered file watcher...");
+    println!("Watching current directory for source code changes");
+    println!("Press Ctrl+C to stop\n");
+    let (tx, rx) = mpsc::channel::<Result<Event>>();
+    let mut watcher = notify::recommended_watcher(tx)?;
+    let mut file_watcher = FileWatcher::new();
+    watcher.watch(Path::new("."), RecursiveMode::Recursive)?;
+
+    for res in rx {
+        match res {
+            Ok(event) => {
+                if let Some(path) = file_watcher.process_event(&event) {
+                    let language = detect_language(&path).unwrap_or("unknown");
+                    let relative_path = path.strip_prefix("./").unwrap_or(&path);
+
+                    println!("file modified: {} ({language})", relative_path.display());
+                }
+            }
+            Err(e) => {
+                println!("file watcher error: {e}");
+            }
+        }
+    }
+    Ok(())
+}
